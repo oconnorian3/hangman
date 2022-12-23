5b61c51458dc3a16cfb5a0bf277abb87bef377cb
oconnorian3 <oconnorian3@gmail.com>
2022-12-23 22:48:03 +0000

    Welcome message plus input for users name and name validator

--- run.py
+++ run.py
@@ -1,3 +1,20 @@
 # Your code goes here.
 # You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+# Write your code to expect a terminal of 80 characters wide and 24 rows hig
+"""
+Welcome message, plus name validator. 
+"""
+startgame = print("How to play. Hangman is a simple word guessing game. Players try to figure out an unknown word by guessing letters. If too many letters which do not appear in the word are guessed, the player is hanged and loses")
+
+while True:
+  try:
+    name = input('What is your name?').upper()
+    if name.isalpha():
+      print('Hi', name, 'Lets play Hangman!')
+      break;
+    else:
+      print("Invalid Name")      
+  except ValueError:
+    print("Provide an Alpha value...")
+    continue
+  
