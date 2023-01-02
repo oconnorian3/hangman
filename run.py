0a9bca79ca6cff0dedc3f68e8e0f816f44b286c5
oconnorian3 <oconnorian3@gmail.com>
2023-01-02 20:40:31 +0000

    Removed minor syntax erorrs after running code through aa validator

--- run.py
+++ run.py
@@ -16,13 +16,19 @@ Start game function
 
 
 def startgame():
-    startgame = print("How to play. Hangman is a simple word guessing game.\n Players try to figure out an unknown word by guessing letters.\n If too many letters which do not appear in the word are guessed,\n the player is hanged and loses")
+    startgame = print("How to play. Hangman is a simple word guessing game."
+                      " Players try to figure out an unknown"
+                      " word by guessing letters."
+                      " If too many letters which"
+                      " do not appear in the word are guessed,"
+                      " the player is hanged and loses")
 
     while True:
         try:
             name = input('What is your name?\n').upper()
             if name.isalpha():
-                print(Fore.LIGHTGREEN_EX + 'Hi', Fore.LIGHTGREEN_EX + name, Fore.LIGHTGREEN_EX + 'Lets play Hangman!')
+                print(Fore.LIGHTGREEN_EX + 'Hi', Fore.LIGHTGREEN_EX +
+                      name, Fore.LIGHTGREEN_EX + 'Lets play Hangman!')
                 break
             else:
                 print(Fore.RED + "Invalid Name")
@@ -42,6 +48,8 @@ def choose_word(words):
         word = random.choice(words)
 
     return word.upper()
+
+
 """
 Function to begin the game itself
 """
@@ -56,9 +64,11 @@ def hangman():
     lives = 7
 
     while len(word_letter) > 0 and lives > 0:
-        print("You have", lives, "lives left and the following letters have been used ", ' '.join(used_letters)) 
+        print("You have", lives, "lives left and the following"
+              " letters have been used ", ' '.join(used_letters))
 
-        word_list = [letter if letter in used_letters else '_' for letter in word]
+        word_list = [letter if letter in used_letters
+                     else '_' for letter in word]
         print(hangman_drawing[lives])
         print('Current Word: ', ' '.join(word_list))
 
@@ -66,44 +76,46 @@ def hangman():
         if user_letter in alphabet - used_letters:
             used_letters.add(user_letter)
             if user_letter in word_letter:
-                word_letter.remove(user_letter)  
-                print("") 
+                word_letter.remove(user_letter)
+                print("")
 
             else:
-                lives = lives -1
-                print ('\nYour Letter,', user_letter, 'is not in the word')     
+                lives = lives-1
+                print('\nYour Letter,', user_letter, 'is not in the word')
 
         elif user_letter in used_letters:
-            print ("\nLetter already used. Please try again")
+            print("\nLetter already used. Please try again")
 
         else:
-            print("\nInvalid Character, Please Try again") 
+            print("\nInvalid Character, Please Try again")
 
     if lives == 0:
         print(hangman_drawing[lives])
         print(Fore.RED + 'You died, sorry. The word was', word)
     else:
-        print(Fore.LIGHTGREEN_EX +'Woo Hoo !!! You guessed the word', word, '!!')
+        print(Fore.LIGHTGREEN_EX +
+              'Woo Hoo !!! You guessed the word', word, '!!')
     """
     Gives user option to restart game
     """
-    user_input = input("Would you like to play hangman again? Type 'Yes' or 'No'\n\n")
+    user_input = input("Would you like to play"
+                       " hangman again? Type 'Yes' or 'No'\n\n")
 
-    if user_input.lower() == "no": 
+    if user_input.lower() == "no":
         quit()
     elif user_input.lower() == "yes":
         clr_terminal()
-        hangman()    
+        hangman()
+
 
 def clr_terminal():
 
     """
    Function to clear screen.
     """
-    os.system('clear')        
+    os.system('clear')
 
 
 if __name__ == '__main__':
     startgame()
-    hangman()             
- 
+    hangman()
